ea31a966308c04b02b0869c0077182c9713a8a83
NolwennDucarmois <nolwenn.ducarnois@condorcet.be>
2024-03-22 17:13:05 +0100

    commit du cours du 22-03-2024 : GestionCourse, méthode modification

--- src/demodb/GestCourse.java
+++ src/demodb/GestCourse.java
@@ -48,7 +48,7 @@ public class GestCourse {
         } while (true);
     }
 
-    public void ajout(){
+    public void ajout() {
         System.out.println("Nom : ");
         String nom = sc.nextLine();
         System.out.println("PriceMoney : ");
@@ -65,53 +65,73 @@ public class GestCourse {
         int km = sc.nextInt();
         String query1 = "insert into APICOURSE(nom, priceMoney, dateCourse, km) values(?,?,?,?)";
         String query2 = "select idCourse from APICOURSE where nom=?";
-        try(PreparedStatement pstm1=dbConnect.prepareStatement(query1);
-            PreparedStatement pstm2=dbConnect.prepareStatement(query2)
-        ){
-            pstm1.setString(1,nom);
-            pstm1.setBigDecimal(2,priceMoney);
+        try (PreparedStatement pstm1 = dbConnect.prepareStatement(query1);
+             PreparedStatement pstm2 = dbConnect.prepareStatement(query2)
+        ) {
+            pstm1.setString(1, nom);
+            pstm1.setBigDecimal(2, priceMoney);
             pstm1.setDate(3, Date.valueOf(dateCourse));
-            pstm1.setInt(4,km);
+            pstm1.setInt(4, km);
             int n = pstm1.executeUpdate();
-            System.out.println(n+" ligne insérée");
-            if(n==1){
+            System.out.println(n + " ligne insérée");
+            if (n == 1) {
                 pstm2.setString(1, nom);
                 ResultSet rs = pstm2.executeQuery();
-                if(rs.next()){
+                if (rs.next()) {
                     int idCourse = rs.getInt(1);
-                    System.out.println("idCourse = "+idCourse);
-                } else{
+                    System.out.println("idCourse = " + idCourse);
+                } else {
                     System.out.println("record introuvable");
                 }
             }
-        }catch (SQLException e){
-            System.out.println("Erreur sql : "+e);
+        } catch (SQLException e) {
+            System.out.println("Erreur sql : " + e);
         }
     }
-    public void recherche(){
+
+    public void recherche() {
 
     }
-    public void modification(){
+
+    public void modification() {
         System.out.println("id de la course à modifier : ");
-
+        int id_rechercher = sc.nextInt();
+        sc.skip("\n");
+        System.out.println("Nouveau priceMoney : ");
+        BigDecimal priceMoney = sc.nextBigDecimal();
+        String query = "update APICOURSE set priceMoney=? where idCourse=?";
+        try (PreparedStatement pstm = dbConnect.prepareStatement(query)) {
+            pstm.setBigDecimal(1, priceMoney);
+            pstm.setInt(2, id_rechercher);
+            int n = pstm.executeUpdate();
+            if (n != 0) {
+                System.out.println(n + " ligne modifiée");
+            } else {
+                System.out.println("Record introuvable");
+            }
+        } catch (SQLException e) {
+            System.out.println("Erreur sql : " + e);
+        }
     }
-    public void suppression(){
+
+    public void suppression() {
         System.out.println("id de la course à supprimer : ");
         int id_rechercher = sc.nextInt();
         String query = "delete from APICOURSE where idCourse = ?";
-        try(PreparedStatement pstm = dbConnect.prepareStatement(query)){
+        try (PreparedStatement pstm = dbConnect.prepareStatement(query)) {
             pstm.setInt(1, id_rechercher);
             int n = pstm.executeUpdate();
-            if(n!=0){
-                System.out.println(n+" ligne supprimée");
-            } else{
+            if (n != 0) {
+                System.out.println(n + " ligne supprimée");
+            } else {
                 System.out.println("Record introuvable");
             }
-        }catch (SQLException e){
-            System.out.println("erreur sql : "+e);
+        } catch (SQLException e) {
+            System.out.println("erreur sql : " + e);
         }
     }
-    public void tous(){
+
+    public void tous() {
 
     }
 
